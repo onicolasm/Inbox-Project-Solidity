b782187d02204ac99fa9928fa39517321441ab74
oscarnicolas <oscarnicolasmozo@gmail.com>
2021-09-22 00:18:24 +0200

    first contract

---
+++ firstContract.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+
+contract simpleStorage{
+    
+    uint256 favoriteNumber = 5;
+    bool favoriteBool = true;
+    string favoriteString = "String";
+    int256 favoriteInt = -5;
+    address favoriteAddress = 0xA13bf3555194c8a65c2Ac6324731852d6Ecf2638;
+    bytes32 favoriteBytes= "cat";
+    
+}
